451ce88380abdcbcbe575e4d7a0f0c19a8ac3c42
ronvid <leon888.lb@gmail.com>
2025-08-16 13:23:31 +0200

    provides edge creation, graph copying and graph deletion

---
+++ GraphUtility.hpp
@@ -0,0 +1,36 @@
+#include <map>
+
+/*
+ * This file adds utility functions for constructing, copying and deleting graphs
+ */
+
+// adds an edge between v and w with cost c
+// since the graph structure does not allow parallel edges, the edge with the smaller cost will be used
+void add_edge(std::map<int, std::map<int, bool>>* G, std::map<int, std::map<int, int>>* W, int v, int w, int c){
+    // check if an edge was already between v-w
+    if((*G)[v].contains(w)){
+        // only overwrite cost if new cost would be lower
+        if(c < (*W)[v][w]){
+            (*W)[v][w] = (*W)[w][v] = c;
+        }
+    }
+    else{
+        // add edge and cost
+        (*G)[v][w] = (*G)[w][v] = true;
+        (*W)[v][w] = (*W)[w][v] = c;
+    }
+}
+
+// copys edges from G to T
+void copy_graph(std::map<int, std::map<int, bool>>* G, std::map<int, std::map<int, bool>>* T){
+    for(const auto& [v, e] : *G){
+        for(const auto& [w, og] : e){
+            (*T)[v][w] = og;
+        }
+    }
+}
+
+// clears values from a given graph
+void delete_graph(std::map<int, std::map<int, bool>>* G){
+    G->clear();
+}
