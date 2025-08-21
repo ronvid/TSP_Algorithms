c3db7ac2f3ba5751b638340e5a09b961afe52246
ronvid <leon888.lb@gmail.com>
2025-08-21 15:51:04 +0200

    added some graphs for testing

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(){
 
     int cycle_cost = 0;
 
-    input = {
+    /*input = {
         { 0 , { {1,true} , {2,true} , {3,true} } },
         { 1 , { {0,true} , {2,true} , {3,true} } },
         { 2 , { {0,true} , {1,true} , {3,true} } },
@@ -25,29 +25,43 @@ int main(){
         { 1 , { {0, 1} , {2, 4} , {3, 6} } },
         { 2 , { {0, 2} , {1, 4} , {3, 5} } },
         { 3 , { {0, 3} , {1, 6} , {2, 5} } }
-    };
+    };*/
 
-    /*input = {
-    { 0 , { {1,true} , {9,true} , {10,true} } },
-    { 1 , { {2,true} , {3,true} , {0,true} } },
-    { 2 , { {1,true} , {3,true} , {10,true} } },
-    { 3 , { {1,true} , {2,true} , {4,true} } },
-    { 4 , { {3,true} , {5,true} , {6,true} } },
-    { 5 , { {4,true} , {6,true} } },
-    { 6 , { {4,true} , {5,true} , {7,true} } },
-    { 7 , { {6,true} , {8,true} , {9,true} } },
-    { 8 , { {7,true} , {9,true} } },
-    { 9 , { {7,true} , {8,true} , {0,true} } },
-    { 10, { {0,true} , {2,true} } }
-    };
-    */
-    /*G = {
-        { 0 , { {1,true} , {2,true} , {3,true} } },
-        { 1 , { {0,true} , {2,true} , {3,true} } },
-        { 2 , { {0,true} , {1,true} , {3,true} } },
-        { 3 , { {0,true} , {1,true} , {2,true} } }
-    };
-    */
+    // baseic graph
+    // add_edge(&input, &input_weights, 0, 1, 1);
+    // add_edge(&input, &input_weights, 1, 2, 2);
+    // add_edge(&input, &input_weights, 2, 3, 3);
+    // add_edge(&input, &input_weights, 3, 4, 4);
+    // add_edge(&input, &input_weights, 4, 5, 5);
+    // add_edge(&input, &input_weights, 5, 6, 6);
+    // add_edge(&input, &input_weights, 6, 7, 7);
+    // add_edge(&input, &input_weights, 7, 8, 8);
+    // add_edge(&input, &input_weights, 8, 9, 9);
+    // add_edge(&input, &input_weights, 9, 0, 10);
+    // add_edge(&input, &input_weights, 1, 3, 11);
+    // add_edge(&input, &input_weights, 4, 6, 12);
+    // add_edge(&input, &input_weights, 7, 9, 13);
+    // add_edge(&input, &input_weights, 0, 10, 14);
+    // add_edge(&input, &input_weights, 10, 2, 15);
+
+    add_edge(&input, &input_weights, 1 , 2 , 1 );
+    add_edge(&input, &input_weights, 1 , 3 , 2 );
+    add_edge(&input, &input_weights, 2 , 3 , 3 );
+    add_edge(&input, &input_weights, 2 , 4 , 4 );
+    add_edge(&input, &input_weights, 3 , 4 , 5 );
+    add_edge(&input, &input_weights, 4 , 5 , 6 );
+    add_edge(&input, &input_weights, 5 , 6 , 7 );
+    add_edge(&input, &input_weights, 5 , 7 , 8 );
+    add_edge(&input, &input_weights, 6 , 7 , 9 );
+    add_edge(&input, &input_weights, 6 , 8 , 10);
+    add_edge(&input, &input_weights, 7 , 8 , 11);
+    add_edge(&input, &input_weights, 8 , 9 , 12);
+    add_edge(&input, &input_weights, 9 , 10, 13);
+    add_edge(&input, &input_weights, 9 , 11, 14);
+    add_edge(&input, &input_weights, 10, 11, 15);
+    add_edge(&input, &input_weights, 10, 12, 16);
+    add_edge(&input, &input_weights, 11, 12, 17);
+    add_edge(&input, &input_weights, 12, 1 , 18);
 
     ShortestHamiltonianCycle(&input, &input_weights, &forced_edges, &cycle_cost);
     //remove(1,2);
