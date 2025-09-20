1b4c097cf661537f320e8e4de19ee3fdd9b70a11
ronvid <leon888.lb@gmail.com>
2025-09-20 19:35:11 +0200

    split up graph utility into header and source file (surely I won't regret this decision later on)

---
+++ GraphUtility.cpp
@@ -0,0 +1,58 @@
+#include "GraphUtility.hpp"
+
+/*
+ * This file adds utility functions for constructing, copying and deleting graphs
+ */
+
+// adds an edge between v and w with cost c
+// since the graph structure does not allow parallel edges, the edge with the smaller cost will be used
+void add_edge(std::unordered_map<int, std::unordered_map<int, int>>* W, int v, int w, int c){
+    // check if an edge was already between v-w
+    if((*W)[v].contains(w)){
+        // only overwrite cost if new cost would be lower
+        if(c < (*W)[v][w]){
+            (*W)[v][w] = (*W)[w][v] = c;
+        }
+    }
+    else{
+        // add edge and cost
+        (*W)[v][w] = (*W)[w][v] = c;
+    }
+}
+
+// copys edges from G to T
+void copy_graph(std::unordered_map<int, std::unordered_map<int, bool>>* G, std::unordered_map<int, std::unordered_map<int, bool>>* T){
+    for(const auto& [v, e] : *G){
+        for(const auto& [w, og] : e){
+            (*T)[v][w] = og;
+        }
+    }
+}
+
+// clears values from a given graph
+void delete_graph(std::unordered_map<int, std::unordered_map<int, bool>>* s){
+    std::cout << s->size() << std::endl;
+    s->clear();
+}
+
+// prints the given graph
+void print_graph(std::unordered_map<int, std::unordered_map<int, bool>>* G){
+    for(const auto& [v, e] : *G){
+        std::cout << v << ": ";
+        for(const auto& [w, og] : e){
+            std::cout << w << "." << og << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// prints the given weight graph
+void print_graph(std::unordered_map<int, std::unordered_map<int, int>>* W){
+    for(const auto& [v, e] : *W){
+        std::cout << v << ": ";
+        for(const auto& [w, og] : e){
+            std::cout << w << "." << og << " ";
+        }
+        std::cout << std::endl;
+    }
+}
--- GraphUtility.hpp
+++ GraphUtility.hpp
@@ -1,58 +1,24 @@
-#include <map>
-#include <iostream>
+#ifndef GRAPH_UTILITY_HPP
+#define GRAPH_UTILITY_HPP
 
-/*
- * This file adds utility functions for constructing, copying and deleting graphs
- */
+#include <map>
+#include <unordered_map>
+#include <iostream>
 
 // adds an edge between v and w with cost c
 // since the graph structure does not allow parallel edges, the edge with the smaller cost will be used
-void add_edge(std::map<int, std::map<int, int>>* W, int v, int w, int c){
-    // check if an edge was already between v-w
-    if((*W)[v].contains(w)){
-        // only overwrite cost if new cost would be lower
-        if(c < (*W)[v][w]){
-            (*W)[v][w] = (*W)[w][v] = c;
-        }
-    }
-    else{
-        // add edge and cost
-        (*W)[v][w] = (*W)[w][v] = c;
-    }
-}
+void add_edge(std::unordered_map<int, std::unordered_map<int, int>>* W, int v, int w, int c);
 
 // copys edges from G to T
-void copy_graph(std::map<int, std::map<int, bool>>* G, std::map<int, std::map<int, bool>>* T){
-    for(const auto& [v, e] : *G){
-        for(const auto& [w, og] : e){
-            (*T)[v][w] = og;
-        }
-    }
-}
+void copy_graph(std::unordered_map<int, std::unordered_map<int, bool>>* G, std::unordered_map<int, std::unordered_map<int, bool>>* T);
 
 // clears values from a given graph
-void delete_graph(std::map<int, std::map<int, bool>>* G){
-    G->clear();
-}
+void delete_graph(std::unordered_map<int, std::unordered_map<int, bool>>* s);
 
 // prints the given graph
-void print_graph(std::map<int, std::map<int, bool>>* G){
-    for(const auto& [v, e] : *G){
-        std::cout << v << ": ";
-        for(const auto& [w, og] : e){
-            std::cout << w << "." << og << " ";
-        }
-        std::cout << std::endl;
-    }
-}
+void print_graph(std::unordered_map<int, std::unordered_map<int, bool>>* G);
 
 // prints the given weight graph
-void print_graph(std::map<int, std::map<int, int>>* W){
-    for(const auto& [v, e] : *W){
-        std::cout << v << ": ";
-        for(const auto& [w, og] : e){
-            std::cout << w << "." << og << " ";
-        }
-        std::cout << std::endl;
-    }
-}
+void print_graph(std::unordered_map<int, std::unordered_map<int, int>>* W);
+
+#endif
