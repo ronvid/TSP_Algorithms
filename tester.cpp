fc62fbcd74fc73a19283838eed3bec9477d587a8
ronvid <leon888.lb@gmail.com>
2025-09-27 21:43:59 +0200

    rudamentary support for schuster

--- tester.cpp
+++ tester.cpp
@@ -1,5 +1,6 @@
 #include "Eppstein.hpp"
 #include "Bruteforce.hpp"
+#include "Schuster.hpp"
 
 #include "Generator.cpp"
 
@@ -7,7 +8,7 @@
 
 // will compare eppstein and bruteforce on a graph of the given size; if always_show is true, additional information will always be shown
 // returns false if the returned costs are not the same and will print additional information
-bool compare_algorithms(int size, bool epp, bool brute, bool always_show=false){
+bool compare_algorithms(int size, bool epp, bool brute, bool schuster, bool always_show=false){
     // success flag
     bool success = true;
 
@@ -19,8 +20,10 @@ bool compare_algorithms(int size, bool epp, bool brute, bool always_show=false){
     // init values
     std::unordered_map<int, std::unordered_map<int, bool>> e_edges;
     std::unordered_map<int, std::unordered_map<int, bool>> b_edges;
+    std::unordered_map<int, std::unordered_map<int, bool>> s_edges;
     int e_cost = 0; bool e_succ;
     int b_cost = 0; bool b_succ;
+    int s_cost = 0; bool s_succ;
 
     std::cout << "Size: " << generated->size() << " Seed: " << seed;
 
@@ -36,8 +39,14 @@ bool compare_algorithms(int size, bool epp, bool brute, bool always_show=false){
         std::cout << " Bruteforce: " << b_cost;
     }
 
+    // Schuster
+    if(schuster){
+        s_succ = Schuster::ShortestHamiltonianCycle(generated, &s_edges, &s_cost);
+        std::cout << " Schuster: " << s_cost;
+    }
+
     // TODO x_succ could be removed: algorithms can fail if graph has no cycle
-    if((e_succ && b_succ && e_cost == b_cost) || !epp || !brute){
+    if((e_succ && b_succ && s_succ && e_cost == b_cost && b_cost == s_cost) || !epp || !brute || !schuster){
         std::cout << " Success!" << std::endl;
     }
     else{
@@ -107,10 +116,15 @@ int main(){
     std::cin >> in;
     if(in == 'n'){ run_bruteforce = false; }
 
+    bool run_schuster = true;
+    std::cout << "Test Schuser? (y/n)" << std::endl;
+    std::cin >> in;
+    if(in == 'n'){ run_schuster = false; }
+
     for(int i = min_size; i < max_size; i++){
         bool stop = false;
         for(int j = 0; j < repetitions; j++){
-            if(!compare_algorithms(i, run_eppstein, run_bruteforce)){
+            if(!compare_algorithms(i, run_eppstein, run_bruteforce, run_schuster)){
                 stop = true;
                 break;
             }
